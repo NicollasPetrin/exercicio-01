808ca36621c21886df6406381f9d202b8beeaa52
NicollasPetrin <134434413+NicollasPetrin@users.noreply.github.com>
2025-03-31 09:24:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var nome = prompt('Insira seu primeiro nome: ')
+var sobrenome = prompt('Insira seu sobrenome: ')
+var idade = parseInt(prompt('Insira sua idade: '))
+
+alert(`Seu nome completo é: ${nome} ${sobrenome} Sua idade é ${idade} e sua data de nascimento é ${2025 - idade}`)
